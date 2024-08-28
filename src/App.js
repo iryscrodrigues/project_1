055e7b24fd9f8ec1dcd980cf7dfe60313a322507
Irys Rodrigues <iryscrodrigues@gmail.com>
2024-08-28 18:24:55 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the Starter Template</h1>
       </header>
+      <main>
+        <img src="https://www.submarinoviagens.com.br/bora-nessa-trip/wp-content/uploads/2019/11/Parque-Nacional-da-Capivara.png" alt="Placeholder" className="responsive-image" />
+        <p className="Text">Serra da Capivara National Park has the largest concentration of prehistoric sites in the Americas (more than 1200), and the largest number of cave paintings in the world (more than 30 thousand paintings)!
+
+The park received the title of Cultural Heritage of Humanity by UNESCO, and in addition to all its importance, its beauty is also something that draws attention.
+
+You will find many canyons, plateaus, mountains and incredible landscapes, it's worth the visit!</p>
+      </main>
+      <Footer />
     </div>
   );
 }
